b143eeb21da1a57cf1a4a5508230945b2c7a163b
kalen510 <kalen.wiley@gmail.com>
2022-03-03 20:24:53 -0500

    model relationships

--- models/index.js
+++ models/index.js
@@ -5,12 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tagged_product',
+  foreignKey: 'product_id',
+  onDelete: 'SET NULL'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'tagged_product',
+  foreignKey: 'tag_id',
+  onDelete: 'SET NULL'
+})
+
 
 module.exports = {
   Product,
